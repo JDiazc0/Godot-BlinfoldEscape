a922b20886ff8e449c01805057a738d06808b100
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2025-05-08 11:09:51 -0500

    feat: update spike collision

--- Scripts/Hazards/Spikes.cs
+++ Scripts/Hazards/Spikes.cs
@@ -3,19 +3,16 @@ using System;
 
 public partial class Spikes : Area2D
 {
-	// Configuración
-	[Export] public float ActiveTime = 2f;
-	[Export] public float InactiveTime = 1.5f;
-	[Export] public float WarningTime = 3f;
-	[Export] public string PlayerGroup = "Player";
+	[Export] public float _activeTime = 2f;
+	[Export] public float _inactiveTime = 1.5f;
+	[Export] public float _warningTime = 3f;
+	[Export] public string _playerGroup = "Player";
 
-	// Componentes
 	private AnimatedSprite2D _sprite;
 	private CollisionShape2D _col;
 	private Timer _stateTimer;
 	private PointLight2D _light;
 
-	// Estados posibles
 	private enum SpikeState { Default, Warning, Attack }
 	private SpikeState _currentState;
 
@@ -24,41 +21,41 @@ public partial class Spikes : Area2D
 		_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		_col = GetNode<CollisionShape2D>("CollisionShape2D");
 		_light = GetNode<PointLight2D>("PointLight2D");
-		
+
 		_stateTimer = new Timer();
 		_stateTimer.OneShot = true;
 		AddChild(_stateTimer);
 		_stateTimer.Timeout += OnTimerTimeout;
 		BodyEntered += OnBodyEntered;
-		
-		SetState(SpikeState.Default); 
+
+		SetState(SpikeState.Default);
 	}
 
 	private void SetState(SpikeState newState)
 	{
 		_currentState = newState;
-		
+
 		switch (_currentState)
 		{
 			case SpikeState.Default:
 				_sprite.Play("default");
 				_col.Disabled = true;
 				_light.Energy = 0f;
-				_stateTimer.Start(InactiveTime);
+				_stateTimer.Start(_inactiveTime);
 				break;
-				
+
 			case SpikeState.Warning:
 				_sprite.Play("warning");
 				_col.Disabled = true;
 				_light.Energy = .7f;
-				_stateTimer.Start(WarningTime);
+				_stateTimer.Start(_warningTime);
 				break;
-				
+
 			case SpikeState.Attack:
 				_sprite.Play("attack");
 				_col.Disabled = false;
 				_light.Energy = 1.5f;
-				_stateTimer.Start(ActiveTime);
+				_stateTimer.Start(_activeTime);
 				break;
 		}
 	}
@@ -70,11 +67,11 @@ public partial class Spikes : Area2D
 			case SpikeState.Default:
 				SetState(SpikeState.Warning);
 				break;
-				
+
 			case SpikeState.Warning:
 				SetState(SpikeState.Attack);
 				break;
-				
+
 			case SpikeState.Attack:
 				SetState(SpikeState.Default);
 				break;
@@ -83,9 +80,9 @@ public partial class Spikes : Area2D
 
 	private void OnBodyEntered(Node2D body)
 	{
-		if (body.IsInGroup(PlayerGroup))
+		if (body.IsInGroup(_playerGroup))
 		{
-			GD.Print("Has Muerto");
+			GameManager.Instance.LoadCurrentLevel();
 		}
 	}
 }
