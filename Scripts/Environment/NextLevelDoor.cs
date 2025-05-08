3eb30e488c541ee99875f1b71da9f1cdcbc46217
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2025-05-08 11:48:14 -0500

    feat: create first level

---
+++ Scripts/Environment/NextLevelDoor.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class NextLevelDoor : Area2D
+{
+	[Export] public string _playerGroup = "Player";
+	private CollisionShape2D _col;
+	
+	public override void _Ready()
+	{
+		_col = GetNode<CollisionShape2D>("CollisionShape2D");
+		BodyEntered += OnBodyEntered;
+	}
+
+	private void OnBodyEntered(Node2D body)
+	{
+		if (body.IsInGroup(_playerGroup))
+		{
+			GameManager.Instance.GoToNextLevel();
+		}
+	}
+}
