369415952ae26b9445736fa7a4373f48f593469a
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2025-05-08 09:01:17 -0500

    feat: create torch and fluctuating light

---
+++ Scripts/Environment/FluctuatingLight.cs
@@ -0,0 +1,46 @@
+using Godot;
+using System;
+
+public partial class FluctuatingLight : Node2D
+{
+	[Export] public PointLight2D _light;
+	private float _minSize = 0.5f;
+	private float _maxSize = 1.3f;
+	private float _minEnergy = 0.5f;
+	private float _maxEnergy = 1.5f;
+	private float _fluctuationSpeed = 1.5f;
+	private Tween _fluctuationTween;
+
+	public override void _Ready()
+	{
+		_light = GetNode<PointLight2D>("PointLight2D");
+
+		StartFluctuation();
+	}
+
+	private void StartFluctuation()
+	{
+		_fluctuationTween?.Kill();
+
+		_fluctuationTween = CreateTween()
+			.SetLoops()
+			.SetEase(Tween.EaseType.InOut)
+			.SetTrans(Tween.TransitionType.Sine);
+
+		_fluctuationTween.TweenProperty(_light, "texture_scale", _maxSize, _fluctuationSpeed)
+			.From(_minSize);
+
+		_fluctuationTween.Parallel()
+			.TweenProperty(_light, "energy", _maxEnergy, _fluctuationSpeed)
+			.From(_minEnergy);
+
+		_fluctuationTween.Chain()
+			.TweenProperty(_light, "texture_scale", _minSize, _fluctuationSpeed)
+			.From(_maxSize);
+
+		_fluctuationTween.Parallel()
+			.TweenProperty(_light, "energy", _minEnergy, _fluctuationSpeed)
+			.From(_maxEnergy);
+	}
+
+}
