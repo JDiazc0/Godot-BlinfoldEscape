d371a3a41170e6a1b511fd0f60231250483721bb
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2025-05-07 23:27:15 -0500

    feat: update color tweener to instance

--- Scripts/Environment/ColorTweener.cs
+++ Scripts/Environment/ColorTweener.cs
@@ -3,26 +3,40 @@ using System;
 
 public partial class ColorTweener : Node2D
 {
-	[Export]
-	public float TransitionDuration = 3f;
+	[Export] public float TransitionDuration = 3f;
 	
 	private CanvasModulate _canvasModulate;
+	private Tween _currentTween;
 	
 	public override void _Ready()
 	{
 		_canvasModulate = GetNode<CanvasModulate>("CanvasModulate");
+		StartDarkening(TransitionDuration);
+	}
+	
+	public void StartDarkening(float duration)
+	{
+		_currentTween?.Kill();
 		_canvasModulate.Color = Colors.White;
 		
-		var tween = CreateTween();
-		
-		tween.TweenProperty(
-			_canvasModulate,
-			"color",
-			Colors.Black,
-			TransitionDuration
-		);
-		
-		tween.SetEase(Tween.EaseType.InOut);
-		tween.SetTrans(Tween.TransitionType.Linear);
+		_currentTween = CreateTween()
+			.SetEase(Tween.EaseType.InOut)
+			.SetTrans(Tween.TransitionType.Linear);
+			
+		_currentTween.TweenProperty(_canvasModulate, "color", Colors.Black, duration);
 	}
+	
+	public void RestoreLight(float restoreDuration)
+	{
+		_currentTween?.Kill();
+		
+		_currentTween = CreateTween()
+			.SetEase(Tween.EaseType.Out)
+			.SetTrans(Tween.TransitionType.Quad);
+			
+		_currentTween.TweenProperty(_canvasModulate, "color", Colors.White, 0f);
+		_currentTween.TweenInterval(restoreDuration);
+		_currentTween.TweenCallback(Callable.From(() => StartDarkening(TransitionDuration)));
+	}
+
 }
