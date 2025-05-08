ee8b4b1c2ffdcef433e7be3b7307bbbf0408c3eb
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2025-05-08 11:04:09 -0500

    feat: create game core and main menu

---
+++ Scripts/Core/GameManager.cs
@@ -0,0 +1,44 @@
+using Godot;
+using System;
+
+public partial class GameManager : Node
+{
+	public static GameManager Instance { get; private set; }
+
+	[Export]
+	public string[] _levels = {
+		"res://Scenes/Level1.tscn"
+	};
+
+	public int _currentLevelIndex { get; private set; } = 0;
+
+	public override void _Ready()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else
+		{
+			QueueFree();
+		}
+	}
+
+	public void LoadCurrentLevel()
+	{
+		GetTree().ChangeSceneToFile(_levels[_currentLevelIndex]);
+	}
+
+	public void GoToNextLevel()
+	{
+		_currentLevelIndex++;
+		if (_currentLevelIndex < _levels.Length)
+		{
+			LoadCurrentLevel();
+		}
+		else
+		{
+			GetTree().ChangeSceneToFile("res://Scenes/MainMenu.tscn");
+		}
+	}
+}
---
+++ Scripts/Core/MainMenu.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+	private Button _startGame;
+	
+	public override void _Ready()
+	{
+		_startGame = GetNode<Button>("StartGame");
+		_startGame.Pressed += OnStartGamePressed;
+	}
+	
+	public void OnStartGamePressed()
+	{
+		GameManager.Instance.LoadCurrentLevel();
+	}
+}
